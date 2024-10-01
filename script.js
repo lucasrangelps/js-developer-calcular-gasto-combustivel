74de99e4cb746911f269d7da51301830755d7eb0
Lucas Rangel <155129810+lucasrangelps@users.noreply.github.com>
2024-10-01 20:16:00 -0300

    update script
    update valor e km por litro etanol

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 let tipoCombustivel = "etanol";
 let valorGasolina = 5.69;
-let valorEtanol = 3.59;
+let valorEtanol = 4.19;
 let kmPorLitroGasolina = 12.3;
-let kmPorLitroEtanol = 8.8;
+let kmPorLitroEtanol = 6.8;
 
 
 const btn = document.querySelector("#send");
