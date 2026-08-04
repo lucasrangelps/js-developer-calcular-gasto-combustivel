124f33ab8589b9d586c91eb2edeea292684410f3
Lucas Rangel <155129810+lucasrangelps@users.noreply.github.com>
2026-08-04 08:16:22 -0300

    Update script.js
    Update valor gasolina e Km por litro

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 let tipoCombustivel = "gasolina";
-let valorGasolina = 6.19;
+let valorGasolina = 6.09;
 let valorEtanol = 4.29;
-let kmPorLitroGasolina = 11;
+let kmPorLitroGasolina = 12.5;
 let kmPorLitroEtanol = 7.67;
 
 
